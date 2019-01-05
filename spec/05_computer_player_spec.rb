3cfa0c54f0b0c4e139ceff6ded493a8fb080b980
Milena Kari <milenakari@yahoo.com>
2019-01-05 19:19:52 +0000

    Automatically backed up by Learn

--- spec/05_computer_player_spec.rb
+++ spec/05_computer_player_spec.rb
@@ -7,9 +7,9 @@ describe 'Players::Computer' do
 
   describe '#move' do
     it 'returns a valid position for the computer to move' do
-      computer = Players::Computer.new("X")
-      game = Game.new
-      board = Board.new
+      #computer = Players::Computer.new("X")
+      game = Game.new(Players::Computer.new("X"),Players::Computer.new("O"))
+      #board = Board.new
 
       valid_moves = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
